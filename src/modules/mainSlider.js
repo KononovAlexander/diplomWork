a3f3c7f565b93a174bcff5caff992adb4be6b936
KononovAlexander <kononov8214@gmail.com>
2021-03-24 22:41:13 +0200

    slider Builded

--- src/modules/mainSlider.js
+++ src/modules/mainSlider.js
@@ -1,6 +1,89 @@
 const mainSlider = () => {
-    console.log('mainSlider: ', mainSlider);
+    const slider = document.querySelector('.top-slider'),
+        slides = slider.querySelectorAll('.item'),
+        text = slider.querySelectorAll('.table'),
+        ul = document.createElement('ul');
+        console.log('text: ', text);
 
+        
+        let currentSlide = 0,
+            interval;
+
+        slider.appendChild(ul);
+        ul.classList.add('slick-dots');
+        ul.style.zIndex = '10';
+
+        const addDots = () => {
+        
+            slides.forEach(() => {
+                const li = document.createElement('li');
+                ul.appendChild(li);
+            });
+            return ul.querySelectorAll('.slick-dots>li');
+        }
+
+        const textVisible = (index) => {
+            text[index].classList.add('active');
+
+        }
+
+        const dots = addDots(); 
+        
+        const dotsActive = (index) => {
+            dots.forEach(dot => dot.classList.remove('slick-active'));
+            dots[index].classList.add('slick-active');
+        }
+
+        const clearSlider = () => slides.forEach(slide => slide.style.display = 'none');
+
+        const nextSlide = (index) =>{
+            slides[index].style.display ='block';
+            dotsActive(index);
+            textVisible(index);
+            currentSlide++;
+        }
+
+        const targetSlide = (index) =>{
+            stopSlider();
+            clearSlider();
+            slides[index].style.display ='block';
+            currentSlide = index;
+            dotsActive(index);
+            startSlider();           
+        }
+            
+        const autoplay = () => {
+            if(currentSlide >= slides.length){
+                currentSlide = 0;
+                nextSlide(currentSlide);
+                dotsActive(0);
+            }else{
+                clearSlider();
+                nextSlide(currentSlide);   
+            }  
+        }
+
+        ul.addEventListener('click', (event) => {
+            let target = event.target;
+            if(!target.classList.contains('slick-active')){
+                dots.forEach((dot, key) => {
+                   if( dot === target){ 
+                       targetSlide(key);    
+                   }  
+                });   
+            }
+        });
+
+        const startSlider = (time = 3000) => {
+            interval = setInterval(autoplay, time);
+        };
+
+        const stopSlider = () => {
+            clearInterval(interval);
+        };
+        nextSlide(0);
+        dotsActive(0);
+        startSlider();
 }
 
 export default mainSlider;
